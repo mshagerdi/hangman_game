704481653ba6d7e1ef554b3a8eca6d8b99f3000c
Mehran Shagerdi <m.shagerdi@yahoo.com>
2024-05-10 17:56:29 +0330

    hint added

---
+++ lib/components/score.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hangman_game/providers/word_data.dart';
+import 'package:provider/provider.dart';
+
+class Score extends StatelessWidget {
+  const Score({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<WordData>(context);
+
+    return Container(
+      margin: EdgeInsets.only(left: 13),
+      child: Text(
+        'Score: ${provider.getScore}',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
